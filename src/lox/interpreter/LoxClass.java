5bce88ef160b0cb3a4153eeff966ce7adcb33bb6
Patrick Kariuki <pkariuk1@jhu.edu>
2025-01-05 20:16:51 +0300

    methods on classes

--- src/lox/interpreter/LoxClass.java
+++ src/lox/interpreter/LoxClass.java
@@ -1,12 +1,15 @@
 package lox.interpreter;
 
 import java.util.List;
+import java.util.Map;
 
-public class LoxClass implements LoxCallable{
+public class LoxClass implements LoxCallable {
   final String name;
+  private final Map<String, LoxFunction> methods;
 
-  LoxClass(String name) {
+  LoxClass(String name, Map<String, LoxFunction> methods) {
     this.name = name;
+    this.methods = methods;
   }
 
   @Override
@@ -23,5 +26,12 @@ public class LoxClass implements LoxCallable{
   public Object call(Interpreter interpreter, List<Object> arguments) {
     LoxInstance instance = new LoxInstance(this);
     return instance;
+  }
+
+  public LoxFunction findMethod(String name) {
+    if (methods.containsKey(name)) {
+      return methods.get(name);
+    }
+    return null;
   }
 }
--- src/lox/interpreter/Resolver.java
+++ src/lox/interpreter/Resolver.java
@@ -16,7 +16,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum FunctionType {
     NONE,
-    FUNCTION
+    FUNCTION,
+    METHOD
   }
 
   public Resolver(Interpreter interpreter) {
@@ -119,6 +120,13 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitClassStmt(Stmt.Class stmt) {
     declare(stmt.name);
     define(stmt.name);
+
+    // Resolve class methods.
+    for (Stmt.Function method : stmt.methods) {
+      FunctionType declaration = FunctionType.METHOD;
+      resolveFunction(method, declaration);
+    }
+
     return null;
   }
 
